782649a60221cc640371657f462fd2538004c7b2
SAM-BOGHARA <88540024+SAM-BOGHARA@users.noreply.github.com>
2022-09-26 21:54:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
 
         <div className="bg-neutral-900">
-            <div className="bg-neutral-900 text-white mx-auto mt-[50px] w-full grow px-4 pt-[72px] md:mt-0 md:max-w-[888px] md:p-6 lg:max-w-screen-xl">
+            <div className="bg-neutral-900 text-white mx-auto md:mt-[50px] w-full grow px-4 pt-[72px] md:mt-0 md:max-w-[888px] md:p-6 lg:max-w-screen-xl">
                 <div className="flex xl:flex-row flex-col sm:flex-col overflow-visible lc-lg:flex-row lc-lg:space-x-4">
                     <LeftSide />
                     <RightSide />
