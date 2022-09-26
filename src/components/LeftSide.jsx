04d9225103eb3d030b0a4a65a366f5dba0d0cd48
Shubham Boghara <shubhamboghara444@gmail.com>
2022-09-26 21:45:58 +0530

    padding error change

--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -10,7 +10,7 @@ function LeftSide() {
         <div  className="bg-neutral-900 rounded-xl">
 
             <div
-                className="left bg-stone-800 shadow-level3 dark:shadow-dark-level3 bg-layer-1 dark:bg-dark-layer-1 rounded-xl flex w-full min-w-[300px] flex-col px-4 pt-[21px] pb-[16px] lc-lg:w-[300px]">
+                className="left bg-stone-800 shadow-level3 dark:shadow-dark-level3 bg-layer-1 dark:bg-dark-layer-1 rounded-xl flex w-full min-w-[300px] flex-col px-4 md:pt-[21px] pb-[16px] lc-lg:w-[300px]">
 
                 <div className="text-label-2 dark:text-dark-label-2 flex flex-col space-y-4">
                     <ProfileCard />
